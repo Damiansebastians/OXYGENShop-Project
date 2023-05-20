8d3e8ad5398d5074541d674a57c4b935b68fa89b
Damiansebastians <116028887+Damiansebastians@users.noreply.github.com>
2023-05-20 11:44:49 +0200

    menu toggle

--- script.js
+++ script.js
@@ -2,21 +2,27 @@
 
 let headerButton = document.getElementById('header-button');
 let menuItems = document.getElementById('menu-items');
-let menuImage1 = document.getElementById('menu_image1');
-let menuImage2 = document.getElementById('menu_image2');
+let menuImage1 = document.getElementById('menu_img_1');
+let menuImage2 = document.getElementById('menu_img_2');
 
 
 function viewMenu() {
     if (menuItems.style.display === "block") {
         menuItems.style.display = "none";
-        menuImage1.style.display = 'none';
-        menuImage2.style.display = 'block';
     } else {
         menuItems.style.display = "block";
-        menuImage1.style.display = 'none';
-        menuImage2.style.display = 'block';
+    }
+}
+
+const changeImage = () => {
+    if (menuImage1.style.display !== "none") {
+        menuImage1.style.display = "none";
+        menuImage2.style.display = "block";
+    } else {
+        menuImage1.style.display = "block";
+        menuImage2.style.display = "none";
     }
 }
 
 headerButton.addEventListener("click", viewMenu);
-
+headerButton.addEventListener('click', changeImage);
