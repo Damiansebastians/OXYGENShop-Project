d39d341eefabe4c56a3afa7669f022d67b0f4d01
Damiansebastians <116028887+Damiansebastians@users.noreply.github.com>
2023-05-24 15:28:42 +0200

    Modal Completed

--- popup.js
+++ popup.js
@@ -1,74 +1,117 @@
-//POPUP - MODAL
 
+//POPUP - MODAL
 let isModalClosed = localStorage.getItem('modalClosed') === 'true';
 let isModalShown = false;
 
 document.addEventListener('DOMContentLoaded', () => {
     let modal = document.getElementById('modal');
+    let modalText = document.getElementById('modal-text');
     let closeBtn = document.getElementById('close-btn');
     let emailInput = document.getElementById('email-input');
     let subscribeBtn = document.getElementById('subscribe-btn');
+    let subscriptionMessage = document.getElementById('subscriptionMessage');
 
-//MOSTRAMOS EL MODAL
-const showModal = () => {
-    modal.style.display = 'block';
+    //MOSTRAMOS EL MODAL
+    const showModal = () => {
+        modal.style.display = 'block';
+        isModalShown = true;
     };
 
-//OCULTAMOS EL MODAL
-const hideModal = () => {
-    modal.style.display = 'none';
+    //OCULTAMOS EL MODAL
+    const hideModal = () => {
+        modal.style.display = 'none';
     };
 
-//MOSTRAR EL MODAL AL CABO DE 5 SEGUNDOS
-const showModalWithDelay = () => {
-    if (isModalClosed !== true && isModalShown !== true) {
-        setTimeout(showModal, 5000);
-    }
-};
+    //MOSTRAR EL MODAL AL CABO DE 5 SEGUNDOS
+    const showModalWithDelay = () => {
+        if (isModalClosed !== true && isModalShown !== true) {
+            setTimeout(showModal, 5000);
+        }
+    };
 
+    //MOSTRAR EL MODAL CUANDO SE BAJA UN 25% DE SCROLL
+    const showModalOnScroll = () => {
+        let scrollPosition = window.innerHeight + window.pageYOffset;
+        let scrollPercentage = (scrollPosition / document.documentElement.scrollHeight) * 100;
 
-//MOSTRAR EL MODAL CUANDO SE BAJA UN 25% DE SCROLL
-const showModalOnScroll = () => {
-    let scrollPosition = window.innerHeight + window.pageYOffset;
-    let scrollPercentage = (scrollPosition / document.documentElement.scrollHeight) * 100;
-    
-    if (scrollPercentage >= 25 && isModalClosed !== true && isModalShown !== true) {
+        if (scrollPercentage >= 25 && isModalClosed !== true && isModalShown !== true) {
             showModal();
             window.removeEventListener('scroll', showModalOnScroll);
-    }
-};
-
-showModalWithDelay();
-window.addEventListener('scroll', showModalOnScroll);
-
-
-
-
-
-    let subscribe = () => {
-      //let email = emailInput.value;
-
-      // Validar la dirección de correo (puedes usar una expresión regular o una API)
-
-      // Enviar el email al servidor (puedes usar AJAX o Fetch API)
-
-      // Ocultar el modal y guardar la información en localStorage
-      hideModal();
-      localStorage.setItem('subscribed', 'true');
+        }
     };
-  
-    closeBtn.addEventListener('click', hideModal);
+
+    showModalWithDelay();
+    window.addEventListener('scroll', showModalOnScroll);
+
+    //CERRAR EL MODAL
+    const closeModal = () => {
+        hideModal();
+        isModalClosed = true;
+        localStorage.setItem('modalClosed', 'true');
+    };
+
+    //LAS 3 FORMAS DE CERRAR EL MODAL
+    closeBtn.addEventListener('click', closeModal);
+
+    window.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            closeModal();
+        }
+    });
+
     modal.addEventListener('click', (event) => {
-      if (event.target === modal) {
-        hideModal();
-      }
+        if (event.target === modal) {
+            closeModal();
+        }
     });
-    document.addEventListener('keydown', (event) => {
-      if (event.key === 'Escape') {
-        hideModal();
-      }
+
+    //MENSAJE DE SUSCRIPCIÓN
+    const showSuccessMessageAndCloseModal = () => {
+        subscriptionMessage.style.display = 'block';
+        modalText.style.display = 'none';
+        emailInput.style.display = 'none';
+        subscribeBtn.style.display = 'none';
+
+        // Guardar la suscripción en el localStorage
+        localStorage.setItem('subscribed', 'true');
+
+        setTimeout(() => {
+            hideModal();
+        }, 2000);
+    };
+
+    //SUSCRIPCIÓN
+    subscribeBtn.addEventListener('click', async () => {
+        //VALIDACIÓN
+        const emailRegex = /^[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+        const email = emailInput.value;
+        let isValid = true;
+
+        if (!emailRegex.test(email)) {
+            emailInput.style.borderColor = "red";
+            isValid = false;
+        }
+
+        //ENVIO DE DATOS
+        if (isValid) {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    },
+                    body: JSON.stringify({ email }),
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log(data);
+                    showSuccessMessageAndCloseModal();
+                }
+
+            } catch (error) {
+                console.error("Error sending the form:", error);
+            }
+        }
     });
-    subscribeBtn.addEventListener('click', subscribe);
-
-
 });
