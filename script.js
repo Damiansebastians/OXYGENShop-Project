d8dc8b57c64108fd7b631b84d11cb9ebd98436cb
Damiansebastians <116028887+Damiansebastians@users.noreply.github.com>
2023-05-26 12:41:55 +0200

    Working un slider

--- script.js
+++ script.js
@@ -167,3 +167,80 @@ const exchange = async () => {
 currencySelect.addEventListener('change', updatePlan);
 exchange();
 
+//--------------------SLIDER----------------------------
+
+class Slider {
+    constructor(id) {
+        this.slider = document.getElementById(id);
+        this.slides = Array.from(this.slider.getElementsByTagName('img'));
+        this.totalSlides = this.slides.length;
+        this.currentSlide = 0;
+        this.intervalId = null;
+        this.showSlide(this.currentSlide);
+        this.addEventListeners();
+        this.autoSlide();
+    }
+
+//MOSTRAR Y OCULTAR IMAGENES
+    showSlide(i) {
+        this.slides.forEach(slide => {
+            slide.style.display = 'none';
+        });
+        this.slides[i].style.display = 'block';
+    }
+
+    nextSlide() {
+      this.currentSlide++;
+      if (this.currentSlide === this.totalSlides) {
+        this.currentSlide = 0;
+      }
+      this.showSlide(this.currentSlide);
+      this.updateActiveDot();
+    }
+  
+    prevSlide() {
+      this.currentSlide--;
+      if (this.currentSlide < 0) {
+        this.currentSlide = this.totalSlides - 1;
+      }
+      this.showSlide(this.currentSlide);
+      this.updateActiveDot();
+    }
+  
+    updateActiveDot() {
+      const dots = Array.from(this.slider.getElementsByClassName('points'));
+      dots.forEach((dot, i) => {
+        if (i === this.currentSlide) {
+          dot.classList.add('active');
+        } else {
+          dot.classList.remove('active');
+        }
+      });
+    }
+  
+    addEventListeners() {
+      const prevBtn = this.slider.querySelector('.prev-btn');
+      prevBtn.addEventListener('click', () => this.prevSlide());
+  
+      const nextBtn = this.slider.querySelector('.next-btn');
+      nextBtn.addEventListener('click', () => this.nextSlide());
+  
+      const dots = Array.from(this.slider.getElementsByClassName('dot'));
+      dots.forEach((dot, i) => {
+        dot.addEventListener('click', () => {
+          this.currentSlide = i;
+          this.showSlide(this.currentSlide);
+          this.updateActiveDot();
+        });
+    });
+    }
+
+//AVANCE AUTOMATICO
+    autoSlide() {
+        setInterval(() => {
+        this.nextSlide();
+        }, 5000);
+    }
+}
+
+const slider = new Slider('slider');
